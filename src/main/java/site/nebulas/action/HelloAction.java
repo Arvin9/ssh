9a00020e107271e0f213c9dcafa8ee3915a2dd57
Arvin9 <594113869@qq.com>
2016-12-02 21:18:14 +0800

    struts2注解。log4j

--- src/main/java/site/nebulas/action/HelloAction.java
+++ src/main/java/site/nebulas/action/HelloAction.java
@@ -1,14 +1,35 @@
 package site.nebulas.action;
 
 import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.ExceptionMapping;
+import org.apache.struts2.convention.annotation.ExceptionMappings;
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.ParentPackage;
+import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.convention.annotation.Results;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by Administrator on 2016/12/1.
  */
+@ParentPackage("struts-default")
+@Namespace("/")
+@Results( { @Result(name = "success", location = "/jsp/main.jsp"),
+            @Result(name = "error", location = "/error.jsp") })
+@ExceptionMappings( {@ExceptionMapping(exception = "Java.lange.RuntimeException", result = "error") })
 public class HelloAction extends ActionSupport{
+    private Logger log = LoggerFactory.getLogger(HelloAction.class);
     @Override
     public String execute() throws Exception {
         System.out.println("hahah");
+        return SUCCESS;
+    }
+    @Action(value = "login", results = { @Result(name = "success", location = "/jsp/main.jsp") })
+    public String login() throws Exception {
+
+        log.info("login");
         return SUCCESS;
     }
 }
